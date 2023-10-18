20eefdb6333bdd19221608bfe1372ca9e80ab694
shivamchaudhary-r <atshivamchaudhary@gmail.com>
2023-10-18 21:16:08 +0530

    latest

--- backend/src/main/java/com/smartroom/backend/service/TeacherServiceImpl.java
+++ backend/src/main/java/com/smartroom/backend/service/TeacherServiceImpl.java
@@ -20,6 +20,7 @@ import java.util.*;
 @Service
 public class TeacherServiceImpl implements TeacherService {
 
+
     private final TeacherRepository teacherRepository;
 
     private final PasswordEncoder passwordEncoder;
@@ -80,108 +81,113 @@ public class TeacherServiceImpl implements TeacherService {
         Student student = getStudentById(studentId);
 
 
+        HashMap<String, String> storedPredictedResult = student.getStudentDetails().getPredictedResult();
+        if (storedPredictedResult == null || storedPredictedResult.get(subject) == null) {
 
-            HashMap<String, String> storedPredictedResult = student.getStudentDetails().getPredictedResult();
-            if (storedPredictedResult == null || storedPredictedResult.get(subject) == null) {
-
-                HashMap<String, ArrayList<Integer>> studentMarks = student.getStudentDetails().getStudentMarks();
-                if (!studentMarks.containsKey(subject) && !subject.equals("overall")) {
-                    throw new InvalidParameter("Invalid subject,Subject not available");
+            HashMap<String, ArrayList<Integer>> studentMarks = student.getStudentDetails().getStudentMarks();
+            if (!studentMarks.containsKey(subject) && !subject.equals("overall")) {
+                throw new InvalidParameter("Invalid subject,Subject not available");
+            } else {
+                ArrayList<Integer> totalMarks;
+                if (studentMarks.containsKey(subject)) {
+                    totalMarks = studentMarks.get(subject);
                 } else {
-                    ArrayList<Integer> totalMarks;
-                    if (studentMarks.containsKey(subject)) {
-                        totalMarks = studentMarks.get(subject);
-                    } else {
-                        totalMarks = getTotalMarks(studentMarks);
-                    }
-
-                    try {
-                        LinkedHashMap<String, Object> params = getMLParams(student.getStudentDetails(), totalMarks);
-                        String predictedResult = getPrediction(params);
-                        storedPredictedResult.put(subject, predictedResult);
-                        student.getStudentDetails().setPredictedResult(storedPredictedResult);
-                        updateStudent(studentId, student.getStudentDetails());
-                        return predictedResult;
-                    } catch (Exception e) {
-                        throw new Exception(e.getMessage());
-                    }
+                    totalMarks = getTotalMarks(studentMarks);
                 }
-            } else {
-                return storedPredictedResult.get(subject);
+
+                try {
+                    LinkedHashMap<String, Object> params = getMLParams(student.getStudentDetails(), totalMarks);
+                    String predictedResult = getPrediction(params);
+                    storedPredictedResult.put(subject, predictedResult);
+                    student.getStudentDetails().setPredictedResult(storedPredictedResult);
+                    updateStudent(studentId, student.getStudentDetails());
+                    return predictedResult;
+                } catch (Exception e) {
+                    throw new Exception(e.getMessage());
+                }
+            }
+        } else {
+            return storedPredictedResult.get(subject);
         }
     }
 
+    @Override
+    public void deleteStudent(String studentId) {
 
-        private String getPrediction(LinkedHashMap < String, Object > params) throws Exception {
-            String url = "http://localhost:80/predict";
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Accept", "application/json");
-            headers.set("Accept", "text/plain");
-            headers.set("Accept", "/");
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setContentLength(345);
-            MlRequest mlRequest = new MlRequest(params);
-            HttpEntity<MlRequest> request = new HttpEntity<>(mlRequest, headers);
-
-            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-            if (response.getStatusCode() == HttpStatus.OK) {
-                return response.getBody();
-
-            } else {
-                throw new Exception("Unable to process request");
-            }
-        }
-
-        private LinkedHashMap<String, Object> getMLParams (StudentDetails
-        studentDetails, ArrayList < Integer > totalMarks){
-            LinkedHashMap<String, Object> params = new LinkedHashMap<>();
-            params.put("sex", studentDetails.getSex());
-            params.put("age", studentDetails.getAge());
-            params.put("address", studentDetails.getAddress());
-            params.put("famsize", studentDetails.getFamilySize());
-            params.put("Pstatus", studentDetails.getParentStatus());
-            params.put("Medu", studentDetails.getMotherEducation());
-            params.put("Fedu", studentDetails.getFatherEducation());
-            params.put("Mjob", studentDetails.getMotherJob());
-            params.put("Fjob", studentDetails.getFatherJob());
-            params.put("traveltime", studentDetails.getTravelTime());
-            params.put("studytime", studentDetails.getStudyTime());
-            params.put("failures", studentDetails.getFailures());
-            params.put("schoolsup", studentDetails.getSchoolSupport());
-            params.put("famsup", studentDetails.getFamilySupport());
-            params.put("paid", studentDetails.getExtraPaidClasses());
-            params.put("activities", studentDetails.getExtraCurricularActivities());
-            params.put("nursery", studentDetails.getNurseryEducation());
-            params.put("higher", studentDetails.getHigherEducation());
-            params.put("internet", studentDetails.getInternet());
-            params.put("famrel", studentDetails.getFamilyRelationship());
-            params.put("freetime", studentDetails.getFreeTime());
-            params.put("health", studentDetails.getFreeTime());
-            params.put("absences", studentDetails.getAbsences());
-            params.put("G1", totalMarks.get(0));
-            params.put("G2", totalMarks.get(1));
-            return params;
-        }
-
-        private ArrayList<Integer> getTotalMarks (HashMap < String, ArrayList < Integer >> studentMarks){
-            ArrayList<Integer> totalMarks = new ArrayList<>();
-            int term1 = 0, term2 = 0;
-
-            int totalSubject = studentMarks.size();
+        teacherRepository.deleteStudent(studentId);
+    }
 
 
-            for (Map.Entry<String, ArrayList<Integer>> entry : studentMarks.entrySet()) {
-                ArrayList<Integer> marks = entry.getValue();
+    private String getPrediction(LinkedHashMap<String, Object> params) throws Exception {
+        String url = "http://localhost:80/predict";
 
-                term1 += marks.get(0);
-                term2 += marks.get(1);
-            }
-            totalMarks.add(term1 / totalSubject);
-            totalMarks.add(term2 / totalSubject);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        headers.set("Accept", "text/plain");
+        headers.set("Accept", "/");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setContentLength(345);
+        MlRequest mlRequest = new MlRequest(params);
+        HttpEntity<MlRequest> request = new HttpEntity<>(mlRequest, headers);
 
-            return totalMarks;
+        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+
+        } else {
+            throw new Exception("Unable to process request");
         }
     }
+
+    private LinkedHashMap<String, Object> getMLParams(StudentDetails
+                                                              studentDetails, ArrayList<Integer> totalMarks) {
+        LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+        params.put("sex", studentDetails.getSex());
+        params.put("age", studentDetails.getAge());
+        params.put("address", studentDetails.getAddress());
+        params.put("famsize", studentDetails.getFamilySize());
+        params.put("Pstatus", studentDetails.getParentStatus());
+        params.put("Medu", studentDetails.getMotherEducation());
+        params.put("Fedu", studentDetails.getFatherEducation());
+        params.put("Mjob", studentDetails.getMotherJob());
+        params.put("Fjob", studentDetails.getFatherJob());
+        params.put("traveltime", studentDetails.getTravelTime());
+        params.put("studytime", studentDetails.getStudyTime());
+        params.put("failures", studentDetails.getFailures());
+        params.put("schoolsup", studentDetails.getSchoolSupport());
+        params.put("famsup", studentDetails.getFamilySupport());
+        params.put("paid", studentDetails.getExtraPaidClasses());
+        params.put("activities", studentDetails.getExtraCurricularActivities());
+        params.put("nursery", studentDetails.getNurseryEducation());
+        params.put("higher", studentDetails.getHigherEducation());
+        params.put("internet", studentDetails.getInternet());
+        params.put("famrel", studentDetails.getFamilyRelationship());
+        params.put("freetime", studentDetails.getFreeTime());
+        params.put("health", studentDetails.getFreeTime());
+        params.put("absences", studentDetails.getAbsences());
+        params.put("G1", totalMarks.get(0));
+        params.put("G2", totalMarks.get(1));
+        return params;
+    }
+
+    private ArrayList<Integer> getTotalMarks(HashMap<String, ArrayList<Integer>> studentMarks) {
+        ArrayList<Integer> totalMarks = new ArrayList<>();
+        int term1 = 0, term2 = 0;
+
+        int totalSubject = studentMarks.size();
+
+
+        for (Map.Entry<String, ArrayList<Integer>> entry : studentMarks.entrySet()) {
+            ArrayList<Integer> marks = entry.getValue();
+
+            term1 += marks.get(0);
+            term2 += marks.get(1);
+        }
+        totalMarks.add(term1 / totalSubject);
+        totalMarks.add(term2 / totalSubject);
+
+        return totalMarks;
+    }
+}
 
 
